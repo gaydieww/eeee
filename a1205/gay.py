c3dba659f7e0e81ef6fa8b2ca24a2c4d4dd45991
gaydieww <s11204212@school.saihs.edu.tw>
2024-12-12 12:04:14 +0800

    Add files via upload

--- a1205/gay.py
+++ a1205/gay.py
@@ -1,4 +1,9 @@
 # Example file showing a basic pygame "game loop"
+from asyncio import events
+from faulthandler import cancel_dump_traceback_later
+from re import T
+from tkinter import Button
+from tracemalloc import stop
 import pygame
 
 # pygame setup
@@ -12,7 +17,7 @@ a_rect.x = 50
 a_rect.y = 300
 b = pygame.transform.scale(b,(100,100))
 b_rect = b.get_rect()
-b_rect.x = 1200
+b_rect.x = 1300
 b_rect.y = 300
 speed=10
 is_jumping = False
@@ -23,8 +28,11 @@ score =0
 font = pygame.font.Font(None,36)
 clock = pygame.time.Clock()
 running = True
+gameover =False
+highscore=0
 
 while running:
+    score +=14
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -33,18 +41,43 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key ==  pygame.K_SPACE:
                 is_jumping = True
-    if is_jumping:
-        a_rect.y -= nowjump
-        nowjump -= g
+            if event.key ==pygame.K_r:
+                score=0
+                b_rect.x = 1500
+                gameover =False
+    if not gameover:
+        if is_jumping:
+            a_rect.y -= nowjump
+            nowjump -= g
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            is_jumping =True
         if a_rect.y > 300:
-            a_rect.y = 300
-            nowjump = jump
+               a_rect.y = 300
+               nowjump = jump
+               is_jumping = False
+              
+            
+        b_rect.x -= speed
+        if b_rect.x < -50:
+                b_rect.x= 1288
+                
+        if a_rect.colliderect(b_rect):
+            if score>highscore:
+                highscore =score
+            gameover = True
+        if gameover == True:
+            speed=0
+            score-=14
+        
+    
 
-            is_jumping = False
-    b_rect.x -= speed
-    if b_rect.x < -30:
-            b_rect.x= 1288
-            score +=1
+
+    
+
+
+
+
+            
 
             
 
@@ -59,6 +92,8 @@ while running:
     screen.blit(score_show,(10,10))
     screen.blit(a,a_rect)
     screen.blit(b,b_rect)
+    highscore_show=font.render(f"HIGH Score:{highscore}",True,(0,0,0))
+    screen.blit(highscore_show,(250,10))
 
     # RENDER YOUR GAME HERE
 

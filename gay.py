226986a2019dd538fd15ede195b5add6b379c69d
gaydieww <s11204212@school.saihs.edu.tw>
2024-12-26 12:02:14 +0800

    死媽

--- gay.py
+++ gay.py
@@ -1,31 +1,32 @@
-from ast import Is
-import pygame
+# Example file showing a basic pygame "game loop"
 from asyncio import events
 from faulthandler import cancel_dump_traceback_later
+from random import random, randrange
 from re import T
 from tkinter import Button
 from tracemalloc import stop
-
+import pygame
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 400))
-img_dino = pygame.image.load("dino.png")
-img_photo= pygame.image.load("photo.jpg")
-img_DinoDuck= [pygame.image.load("DinoDuck1.png"),pygame.image.load("DinoDuck2.png")]
-img_DinoRun = [pygame.image.load("DinoRun1.png"),pygame.image.load("DinoRun2.png")]
-img_Bird = [pygame.image.load("Bird1.png"),pygame.image.load("Bird2.png")]
-
-dino_rect = img_dino.get_rect()
-dino_rect.x = 50
-dino_rect.y = 300
-
-photo_rect = img_photo.get_rect()
-photo_rect.x = 1300
-photo_rect.y = 300
-
-
-speed=20
+a = pygame.image.load("padoru.png")
+b = pygame.image.load("photo.jpg")
+c = pygame.image.load("good.jpeg")
+img_track = pygame.image.load("track.png")
+a = pygame.transform.scale(a,(100,100))
+a_rect = a.get_rect()
+a_rect.x = 50
+a_rect.y = 300
+b = pygame.transform.scale(b,(100,100))
+b_rect = b.get_rect()
+b_rect.x = 1300
+b_rect.y = 300
+c = pygame.transform.scale(c,(150,150))
+c_rect = c.get_rect()
+c_rect.x = 2000
+c_rect.y = 500
+speed=10
 is_jumping = False
 jump = 28
 nowjump =jump
@@ -36,9 +37,10 @@ clock = pygame.time.Clock()
 running = True
 gameover =False
 highscore=0
-lastime=10
-frame=1
-is_ducking=False
+random
+speedlist = [5,6,7,8,9]
+
+level=0
 while running:
     
     # poll for events
@@ -47,71 +49,59 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+               a_rect.y = 300
             if event.key ==  pygame.K_SPACE:
-                is_jumping = True
-            if event.type  == pygame.K_r:
-                running = True
-                score=0
-                photo_rect.x = 1500
-                gameover =False
-                photo_rect.x -= speed 
-                
-            if event.type == pygame.K_DOWN:
-                dino_rect.y =330
-                is_ducking=True
-        if event.type == pygame.KEYUP:
-            if is_ducking:
-                dino_rect.y =300
-                is_ducking=False
-          
-    if not gameover:
+               is_jumping = True
+            if event.key ==pygame.K_r:
+               score=0
+               b_rect.x = 1500
+               c_rect.x = 2300
+               gameover =False
+               running = True
+    if  gameover==False:
         score +=14
         if is_jumping:
-            dino_rect.y -= nowjump
-            nowjump -= g
+            a_rect.y -= nowjump
+            nowjump -= g 
+            
+        if gameover == True:
+            speed=0
         if event.type == pygame.MOUSEBUTTONDOWN:
             is_jumping =True
-        if dino_rect.y > 300:
-               dino_rect.y = 300
+        if a_rect.y > 300:
+               a_rect.y = 300
                nowjump = jump
                is_jumping = False
         
-        photo_rect.x-=speed    
-        photo_rect.x -= speed
-        if photo_rect.x < -50:
-            photo_rect.x= 2500
-        if Bird_rect.x < -50:
-            Bird_rect.x= 2000
+        c_rect.x -= speed
+        b_rect.x -= speed
+        if b_rect.x < -50:
+            b_rect.x = 1300
+        if c_rect.x < -50:
+            c_rect.x = 1500
 
-                
-        if dino_rect.colliderect(photo_rect):
+
+        if a_rect.colliderect(b_rect):
             if score>highscore:
                 highscore =score
             gameover = True
-        if dino_rect.colliderect(Bird_rect):
+        if a_rect.colliderect(c_rect):
             if score>highscore:
                 highscore =score
             gameover = True
-        if gameover == True:
+        if score >3000:
+            speed = speedlist[3]
+            level = 3
+        elif score >2000:
+            speed = speedlist[2]
+            level =2
+        elif score>1000:
+            speed = speedlist[1]
+            level = 1
             
-            speed=0
-            score-=14
         
-        nowtime =pygame.time.get_ticks()
-        if nowtime - lastime > 300:
-            frame =(frame+1)%2
-            lastime =nowtime
-
-        if is_ducking: 
-            screen.blit(img_DinoDuck,DinoDuck_rect)
-        else:
-            screen.blit(img_Bird,Bird_rect)
-        pygame.display.flip()
-          
-
-            
-
-        
+    
 
 
     
@@ -132,11 +122,15 @@ while running:
     screen.fill("white")
     score_show=font.render(f"Score:{score}",True,(0,0,0))
     screen.blit(score_show,(10,10))
-    screen.blit(img_dino,dino_rect)
-    screen.blit(img_photo,photo_rect)
+    screen.blit(a,a_rect)
+    screen.blit(b,b_rect)
     highscore_show=font.render(f"HIGH Score:{highscore}",True,(0,0,0))
     screen.blit(highscore_show,(250,10))
-    screen.blit(score_show,(10,10))
+    level_show = font.render(f"Level: {level}",True, (0,0,0))
+    screen.blit(level_show,(10,50))
+    screen.fill((255,255,255))
+    screen.blit(img_track,(0,370))
+
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
